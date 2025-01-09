3cfe54a20207dbd9968a0baef38a2476ca4f4102
Johnson Samuel <samthnalyst@gmail.com>
2025-01-09 22:00:09 +0100

    Update wsgi.py

--- avantlush_backend/avantlush_backend/wsgi.py
+++ avantlush_backend/avantlush_backend/wsgi.py
@@ -14,4 +14,4 @@ from whitenoise import WhiteNoise
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'avantlush_backend.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root='static/')
+application = WhiteNoise(application, root='staticfiles')
