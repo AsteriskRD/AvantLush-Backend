946b6872aff1e6bbbcb79dc5ab8f7f2ada86f63e
Samuel <samthanalyst@gmail.com>
2025-01-11 09:11:06 +0100

    Updates with recent changes

--- avantlush_backend/avantlush_backend/wsgi.py
+++ avantlush_backend/avantlush_backend/wsgi.py
@@ -11,7 +11,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'avantlush_backend_main.avantlush_backend.settings')
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'avantlush_backend.settings')
 application = get_wsgi_application()
 application = WhiteNoise(application, root='static/')
--- build.sh
+++ build.sh
@@ -2,7 +2,13 @@
 # exit on error
 set -o errexit
 
+# Install Python dependencies
+pip install --upgrade pip
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+# Run migrations and collect static files
 python manage.py migrate
+python manage.py collectstatic --no-input
+
+# Create cache table for sessions
+python manage.py createcachetable
