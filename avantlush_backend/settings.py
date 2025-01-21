e5e202ddbbec794c250597777feecee700497652
Samuel <samthanalyst@gmail.com>
2025-01-21 12:06:49 +0100

    Added health check to database

--- avantlush_backend/settings.py
+++ avantlush_backend/settings.py
@@ -181,10 +181,12 @@ ACCOUNT_UNIQUE_EMAIL = True
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# Database configuration
 DATABASES = {
     'default': dj_database_url.config(
         default='sqlite:///db.sqlite3',
-        conn_max_age=600
+        conn_max_age=600,
+        conn_health_checks=True,  # Add health checks for better reliability
     )
 }
 
