955eae52965dc9e79ea5ced5acc1089b7c34d6f7
JOHNSON SAMUEL <samthnalyst@gmail.com>
2025-03-17 07:59:22 -0700

    Adding Superusers

--- avantlush_backend/api/management/commands/ensure_superuser.py
+++ avantlush_backend/api/management/commands/ensure_superuser.py
@@ -2,23 +2,37 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 
 class Command(BaseCommand):
-    help = 'Ensures superuser exists'
+    help = 'Ensures superusers exist'
 
     def handle(self, *args, **kwargs):
         User = get_user_model()
-        email = 'samthnalyst@gmail.com'
         
-        if not User.objects.filter(email=email).exists():
-            User.objects.create_superuser(
-                email=email,
-                password='Sam787878',
-                is_staff=True,
-                is_superuser=True
-            )
-            self.stdout.write(self.style.SUCCESS(f'Superuser {email} created'))
-        else:
-            user = User.objects.get(email=email)
-            user.is_staff = True
-            user.is_superuser = True
-            user.save()
-            self.stdout.write(self.style.SUCCESS(f'Superuser {email} updated'))
+        superusers = [
+            {
+                'email': 'samthnalyst@gmail.com',
+                'password': 'Sam787878',
+            },
+            {
+                'email': 'danieludechukwu117@gmail.com',
+                'password': 'Dan1234',  
+            }
+        ]
+        
+        for superuser in superusers:
+            email = superuser['email']
+            password = superuser['password']
+            
+            if not User.objects.filter(email=email).exists():
+                User.objects.create_superuser(
+                    email=email,
+                    password=password,
+                    is_staff=True,
+                    is_superuser=True
+                )
+                self.stdout.write(self.style.SUCCESS(f'Superuser {email} created'))
+            else:
+                user = User.objects.get(email=email)
+                user.is_staff = True
+                user.is_superuser = True
+                user.save()
+                self.stdout.write(self.style.SUCCESS(f'Superuser {email} updated'))
