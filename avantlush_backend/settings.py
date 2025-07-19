d9ea51c699ab8390eaafa1bfcb6b19fd98a46267
JOHNSON SAMUEL <samthnalyst@gmail.com>
2025-07-19 23:39:21 +0100

    Token delay updated

--- avantlush_backend/settings.py
+++ avantlush_backend/settings.py
@@ -265,7 +265,7 @@ REST_AUTH = {
 
 # JWT settings
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),  # Changed from 1 hour to 1 day
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
